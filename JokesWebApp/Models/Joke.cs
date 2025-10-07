0822d2eb2c9700475a345d0acd7f3f4572f80701
Rohan Adhav <rohanadhav78@gmail.com>
2025-10-07 21:35:30 +0530

    Initial commit - JokesWebApp project

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+    }
+}
